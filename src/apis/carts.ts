ab673098b1b06472fbdaa473dc92b2520a2cdac9
nmhung1210 <nmhung1210@gmail.com>
2019-03-06 18:18:32 +0700

    adding cart apis

--- src/apis/carts.ts
+++ src/apis/carts.ts
@@ -3,3 +3,7 @@ import api from './webApi';
 export function createCart (): Promise<string> {
   return api.get('/shoppingcart/generateUniqueId').then(({cart_id}) => cart_id);
 }
+
+export function addProduct () {
+  // todo
+}
