0ef7926f4076435482e40a6306c88e9ab19515d3
nmhung1210 <nmhung1210@gmail.com>
2019-03-11 13:08:54 +0700

    Adding docs for serving the website by github.

---
+++ docs/precache-manifest.54a5b571de33ea7a1f99715458655e01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "0e8bbe566ebf42259a6b",
+    "url": "/static/js/main.0e8bbe56.chunk.js"
+  },
+  {
+    "revision": "e4fba76e77f9e3091fa5",
+    "url": "/static/js/2.e4fba76e.chunk.js"
+  },
+  {
+    "revision": "0e8bbe566ebf42259a6b",
+    "url": "/static/css/main.57191bc9.chunk.css"
+  },
+  {
+    "revision": "717713dfb9ea3e9d5a4ba6f766ffe0ba",
+    "url": "/index.html"
+  }
+];
